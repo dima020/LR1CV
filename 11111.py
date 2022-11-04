90e50a2c3620a7e0495e7bdb1f9fe07cac40e8d8
dima020 <37048143+dima020@users.noreply.github.com>
2022-11-04 10:11:21 +0300

    commit
    04.11.22
    10-11

--- 11111.py
+++ 11111.py
@@ -1,28 +1,70 @@
 import cv2
 import numpy as np
 from PIL import Image
-from numpy import asarray
+from numpy import count_nonzero
 
-def qqq(img1, img2):
-    img1k=asarray(img1)
-    img2k=asarray(img2)
-    k = 0
-    for i in img1k.shape[0]:
-        for j in img1k.shape[1]:
-            if img1k[i, j] == img2k[i, j]:
-                k = k + 1
-    return 1 - k / (img1k.shape[0] * img1k.shape[1])
+# def qqq(img1, img2):
+#     img1k=asarray(img1)
+#     img2k=asarray(img2)
+#     k = 0
+#     for i in img1k.shape[0]:
+#         for j in img1k.shape[1]:
+#             if img1k[i, j] == img2k[i, j]:
+#                 k = k + 1
+#     return 1 - k / (img1k.shape[0] * img1k.shape[1])
 
 
 img1 = cv2.imread('outputs/1.1_result.jpg')
-img2 = cv2.imread('outputs/2.3_result.jpg')
+img2 = cv2.imread('outputs/1.2_result.jpg')
 img3 = cv2.imread('outputs/1.3_result.jpg')
 
-img = Image.open('outputs/1.1_result.jpg')
+img12 = cv2.imread('outputs/2.1_result.jpg')
+img22= cv2.imread('outputs/2.2_result.jpg')
+img32 = cv2.imread('outputs/2.3_result.jpg')
+
+img13 = cv2.imread('outputs/3.1_result.jpg')
+img23 = cv2.imread('outputs/3.2_result.jpg')
+img33 = cv2.imread('outputs/3.3_result.jpg')
 
 
-#result = qqq(img1,img2)
-result1 = cv2.matchTemplate(img1, img3, cv2.TM_CCOEFF_NORMED)
+result = np.array(img1-img2)
+result1=count_nonzero(result)
+print(result1/(result.shape[0]*result.shape[1]))
 
-print(result1)
+result = np.array(img1-img3)
+result1=count_nonzero(result)
+print(result1/(result.shape[0]*result.shape[1]))
+
+result = np.array(img2-img3)
+result1=count_nonzero(result)
+print(result1/(result.shape[0]*result.shape[1]))
+
+
+result = np.array(img12-img22)
+result1=count_nonzero(result)
+print(result1/(result.shape[0]*result.shape[1]))
+
+result = np.array(img12-img32)
+result1=count_nonzero(result)
+print(result1/(result.shape[0]*result.shape[1]))
+
+result = np.array(img22-img32)
+result1=count_nonzero(result)
+print(result1/(result.shape[0]*result.shape[1]))
+
+##
+result = np.array(img13-img23)
+result1=count_nonzero(result)
+print(result1/(result.shape[0]*result.shape[1]))
+
+result = np.array(img13-img33)
+result1=count_nonzero(result)
+print(result1/(result.shape[0]*result.shape[1]))
+
+result = np.array(img23-img33)
+result1=count_nonzero(result)
+print(result1/(result.shape[0]*result.shape[1]))
+
+
+
 
